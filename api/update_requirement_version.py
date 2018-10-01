9d9c1969351e8027c3d9090d6f45987964950687
Damien Goldenberg <dgoldenberg@vente-privee.com>
2018-10-01 14:03:16 +0200

    Merge pull request #67 from Lazersmoke/fix-andys-shit Fix andys shit

--- api/update_requirement_version.py
+++ api/update_requirement_version.py
@@ -1,7 +1,7 @@
 import re
 
 requirements = open("app/setup.py").read()
-version = re.search("VERSION = '\d+\.\d+.\d+'", requirements).group()
+version = re.search("VERSION *= *'(\d+\.\d+.\d+)'", requirements).groups()[0].split(".")
 open("app/setup.py", "w").write(
     re.sub("VERSION = '\d+\.\d+.\d+'",
            "VERSION = {}.{}.{}".format(version[0], str(int(version[1]) + 1), version[2]),
