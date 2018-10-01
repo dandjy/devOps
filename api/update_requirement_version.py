37cf6b12c3cb61233c33df46dde0c14ebb866834
Damien Goldenberg <dgoldenberg@vente-privee.com>
2018-10-01 12:54:37 +0200

    I just evaluated random code in my console

---
+++ api/update_requirement_version.py
@@ -0,0 +1,9 @@
+import re
+
+requirements = open("app/setup.py").read()
+version = re.search("VERSION = '\d+\.\d+.\d+'", requirements).group()
+open("app/setup.py", "w").write(
+    re.sub("VERSION = '\d+\.\d+.\d+'",
+           "VERSION = {}.{}.{}".format(version[0], str(int(version[1]) + 1), version[2]),
+           requirements)
+)
