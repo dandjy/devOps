99ba259202a4dfed48e0ff2219a1fbc996211438
Damien Goldenberg <dgoldenberg@vente-privee.com>
2018-10-01 14:16:47 +0200

    Merging 'WIP: Do Not Merge This Branch' Into Master

--- api/update_requirement_version.py
+++ api/update_requirement_version.py
@@ -4,6 +4,6 @@ requirements = open("app/setup.py").read()
 version = re.search("VERSION *= *'(\d+\.\d+.\d+)'", requirements).groups()[0].split(".")
 open("app/setup.py", "w").write(
     re.sub("VERSION = '\d+\.\d+.\d+'",
-           "VERSION = {}.{}.{}".format(version[0], str(int(version[1]) + 1), version[2]),
+           "VERSION = '{}.{}.{}'".format(version[0], str(int(version[1]) + 1), version[2]),
            requirements)
 )
